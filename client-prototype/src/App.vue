73c126a0ed6a681fc8c40ef36d9fa6941f114e06
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2026-02-17 22:42:12 +0100

    Add error page for when config loading fails

--- client-prototype/src/App.vue
+++ client-prototype/src/App.vue
@@ -1,13 +1,25 @@
 <template>
-  <router-view></router-view>
+  <div v-if="config.error" class="app-content p-0 d-flex flex-column align-items-center">
+    <h1 class="display-5">Solaris: Command</h1>
+
+    <p class="lead">An error occurred while loading the application. Solaris:Command might be down for maintenance. Please try reloading the page later:</p>
+    <div>
+      <button @click="reload" class="btn btn-primary">Reload</button>
+    </div>
+  </div>
+  <router-view v-else></router-view>
 </template>
 
 <script setup lang="ts">
-import { onMounted } from "vue";
+import {inject, onMounted} from "vue";
 import { useAuthStore } from "./stores/auth";
-import NavBar from "./components/NavBar.vue";
+import {configInjectionKey} from "@/utils/config.ts";
+
+const config = inject(configInjectionKey)!;
 
 const authStore = useAuthStore();
+
+const reload = () => window.location.reload();
 
 onMounted(() => {
   authStore.initialize();
